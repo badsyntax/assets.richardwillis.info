8ef41c55774724392093a9439060c79454c2884b
Richard Willis <willis.rh@gmail.com>
2020-12-27 19:16:06 +0000

    Add photos and GitHub Actions workflow

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # assets.richardwillis.info
+
+After copying files into photos, be sure to run `./scripts/resize-root-images.sh`;
+
+## GitHub Actions
+
+### Secrets
+
+The following secrets need to be set:
+
+- `AWS_ACCESS_KEY_ID`
+- `AWS_S3_BUCKET`
+- `AWS_SECRET_ACCESS_KEY`
+
+### Resizing and syncing to S3
+
+Images are resized (using ImageMagick) and synced to S3 when a pull request is created.
---
+++ scripts/resize-responsive-image.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+
+IMAGE="$1"
+SIZES=("300" "768" "1280")
+FILENAME=$(basename -- "$IMAGE")
+EXTENSION="${FILENAME##*.}"
+
+log_info() {
+  local MESSAGE=$1
+  echo "----> $MESSAGE"
+}
+
+log_error() {
+  local MESSAGE=$1
+  echo "!! $MESSAGE"
+}
+
+if [[ "$EXTENSION" == "jpg" ]]; then
+  log_info "Resizing $IMAGE"
+  for size in "${SIZES[@]}"; do
+    convert "$IMAGE" \
+      -resize "$size" \
+      -set filename:t '%d/resized/%t'"-$size" '%[filename:t].jpg'
+  done
+else
+  log_error "Not a jpg image"
+  exit 1
+fi
